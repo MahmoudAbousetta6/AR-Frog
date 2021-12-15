877e38ff047860f80ca8924550c8967fbc3a84c0
MahmoudAbousetta <55044275+MahmoudAbousetta6@users.noreply.github.com>
2021-12-15 10:37:45 +0200

    Rename Follow Camera

--- Assets/Scripts/Placed Objects/FollowCamera.cs
+++ Assets/Scripts/Placed Objects/FollowCamera.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FaceCamera : MonoBehaviour
+public class FollowCamera : MonoBehaviour
 {
     [SerializeField] private Transform target = null;
 
